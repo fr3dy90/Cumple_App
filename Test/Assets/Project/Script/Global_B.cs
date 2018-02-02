bb3b854770687e6db969c00c2db0bfe5f4bc6ebf
fredy <jfr3dy90@gmail.com>
2018-02-02 15:40:07 -0500

    Intro y Scriptable OBJ
    Se creo la introduccion y los Objetos scriptables

---
+++ Test/Assets/Project/Script/Global_B.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Global_B
+{ 
+	public static IEnumerator Fade(float from, float to, float speed, CanvasGroup fade)
+    {
+        fade.alpha = from;
+        while(fade.alpha != to)
+        {
+            yield return null;
+            fade.alpha = Mathf.MoveTowards(fade.alpha, to, Time.deltaTime / speed);
+        }
+    }
+}
---
+++ Test/Assets/Project/Script/Intro.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class Intro : MonoBehaviour {
+
+    public CanvasGroup fade;
+    public VuforiaBehaviour vuforiaB;
+    public float secondsWait;
+    bool call;
+    public float fadeWait;
+
+    private void Awake()
+    {
+        fade = FindObjectOfType<CanvasGroup>();
+        call = true;
+    }
+
+    void Start () {
+        fade.alpha = 1;
+        StartCoroutine(Global_B.Fade(1, 0, fadeWait, fade));
+	}
+	
+	void Update () {
+		if(vuforiaB == null)
+        {
+            vuforiaB = FindObjectOfType<VuforiaBehaviour>();
+            vuforiaB.enabled = false;
+        }
+        if (secondsWait > 0)
+        {
+            secondsWait -= Time.deltaTime;
+        }
+        else
+        {
+            if (call)
+            {
+                ChangeScene();
+            }
+        }
+    }
+
+    void ChangeScene()
+    {
+        call = false;
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return StartCoroutine(Global_B.Fade(0, 1, fadeWait, fade));
+        print("Change");
+    }
+}
